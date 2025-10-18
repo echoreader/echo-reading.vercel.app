05fe4e9b06bd9cd4164bd168e28e5d97b8671bc3
echoreader <tom.bisnis@gmail.com>
2025-10-19 01:22:06 +0700

    Rename Import again

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./header";
+import Footer from "./footer";
 import "../styles/global.css"; // satu CSS untuk semua
 
 export default function Layout({ children }) {
