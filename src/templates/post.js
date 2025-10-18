f792371ac50b1beea05f3ce72026a1243db38bf4
Echo Reader <tom.bisnis@gmail.com>
2025-10-19 01:39:03 +0700

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 
 export const query = graphql`
   query PostBySlug($slug: String!) {
